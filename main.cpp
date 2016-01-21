a9e76d3888af47f7d7cee3b1f1318aec7f282649
Matt Michaelis <matt-mich@users.noreply.github.com>
2016-01-21 15:30:27 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 /**
  * File: main.cpp
  * Project: ucd-csci2312-pa0
- * Author: Ivo Georgiev
- * Date: 2016-01-18
+ * Author: Ivo Georgiev, modified by Matthew Michaelis
+ * Date: 2016-01-21
  */
 #include <iostream>
 
 int main() {
-    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
+    
+    // My student ID number modulo 13 results as 7
+    std::cout << "Hello, world! My number is " << 7 << '!' << std::endl;
 
     return 0;
 }
